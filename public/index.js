6d2e3a79f6e0cecec675d7be98405427941a286b
Penny Chen <rainbow801122@gmail.com>
2022-11-23 22:23:09 +0800

    feat : 完成search failed js code

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@ let content = document.querySelector(".content");
 let button = document.querySelector(".btn");
 let input = document.querySelector(".input1");
 let category = document.querySelector(".category");
+let fail = document.querySelector(".fail");
 
 window.onload = observe(0, "");
 
@@ -31,37 +32,42 @@ function observe(page, keyword) {
           .then(function (data) {
             let clist = data.data;
             // console.log(clist[0].images[0]);
-            for (let i = 0; i < clist.length; i++) {
-              let imageDiv = document.createElement("div");
-              imageDiv.setAttribute("class", "image");
-              let titleDiv = document.createElement("div");
-              titleDiv.setAttribute("class", "title");
+            if (clist != "") {
+              for (let i = 0; i < clist.length; i++) {
+                let imageDiv = document.createElement("div");
+                imageDiv.setAttribute("class", "image");
+                let titleDiv = document.createElement("div");
+                titleDiv.setAttribute("class", "title");
 
-              //最內層Div
-              let mrtText = document.createTextNode(clist[i].mrt);
-              let mrtDiv = document.createElement("div");
-              mrtDiv.setAttribute("class", "mrt");
-              mrtDiv.appendChild(mrtText);
-              let catText = document.createTextNode(clist[i].category);
-              let catDiv = document.createElement("div");
-              catDiv.setAttribute("class", "cat");
-              catDiv.appendChild(catText);
-              //放入titleDiv
-              titleDiv.appendChild(mrtDiv);
-              titleDiv.appendChild(catDiv);
+                //最內層Div
+                let mrtText = document.createTextNode(clist[i].mrt);
+                let mrtDiv = document.createElement("div");
+                mrtDiv.setAttribute("class", "mrt");
+                mrtDiv.appendChild(mrtText);
+                let catText = document.createTextNode(clist[i].category);
+                let catDiv = document.createElement("div");
+                catDiv.setAttribute("class", "cat");
+                catDiv.appendChild(catText);
+                //放入titleDiv
+                titleDiv.appendChild(mrtDiv);
+                titleDiv.appendChild(catDiv);
 
-              let imgTag = document.createElement("img");
-              imgTag.setAttribute("src", clist[i].images[0]);
+                let imgTag = document.createElement("img");
+                imgTag.setAttribute("src", clist[i].images[0]);
 
-              let attractionNameText = document.createTextNode(clist[i].name);
-              let attractionNameDiv = document.createElement("div");
-              attractionNameDiv.setAttribute("class", "attractionName");
-              attractionNameDiv.appendChild(attractionNameText);
+                let attractionNameText = document.createTextNode(clist[i].name);
+                let attractionNameDiv = document.createElement("div");
+                attractionNameDiv.setAttribute("class", "attractionName");
+                attractionNameDiv.appendChild(attractionNameText);
 
-              imageDiv.appendChild(imgTag);
-              imageDiv.appendChild(attractionNameDiv);
-              imageDiv.appendChild(titleDiv);
-              document.querySelector(".content").appendChild(imageDiv);
+                imageDiv.appendChild(imgTag);
+                imageDiv.appendChild(attractionNameDiv);
+                imageDiv.appendChild(titleDiv);
+                document.querySelector(".content").appendChild(imageDiv);
+                fail.style.display = "none";
+              }
+            } else {
+              fail.style.display = "flex";
             }
             if (data.nextPage != null) {
               page = data.nextPage;
